af1dd8d977ffa877b5a6149e129c04d3b7c780c2
dlynch34 <dlynch34@gmail.com>
2025-03-31 08:51:37 -0400

    Create zti_backup.ps1

---
+++ zti_backup.ps1
@@ -0,0 +1,20 @@
+Write-Host  -ForegroundColor Cyan "Importing the sweet OSD PowerShell Module"
+Import-Module OSD -Force
+
+#TODO: Spend the time to write a function to do this and put it here
+Write-Host  -ForegroundColor Cyan "Ejecting ISO"
+Write-Warning "That didn't work because I haven't coded it yet!"
+#Start-Sleep -Seconds 5
+
+#Start OSDCloud ZTI the RIGHT way
+Write-Host  -ForegroundColor Cyan "Start OSDCloud with MY Parameters"
+Start-OSDCloud -OSLanguage en-us -OSBuild 24H2 -OSEdition Enterprise -ZTI
+
+#Anything I want  can go right here and I can change it at any time since it is in the Cloud!!!!!
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud PostAction ..."
+Write-Warning "I'm not sure of what to put here yet"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
