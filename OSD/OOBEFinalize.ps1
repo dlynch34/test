5e159b21905c1f059832f0d42a91b29504a12f2a
dlynch34 <dlynch34@gmail.com>
2025-04-09 14:09:58 -0400

    Update OOBEFinalize.ps1

--- OSD/OOBEFinalize.ps1
+++ OSD/OOBEFinalize.ps1
@@ -4,15 +4,6 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 Write-Host "Installing required PowerShell modules for OOBE tasks..." -ForegroundColor Cyan
 Install-Module -Name OSD -Force -Verbose
 
-# Create log file
-$LogFile = "C:\ProgramData\RenameComputer.log"
-Function Write-Log {
-    param ([string]$Message)
-    $TimeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    Add-Content -Path $LogFile -Value "$TimeStamp - $Message"
-}
-Write-Log "Starting OOBE Finalize Script"
-
 
 # Enable FIPS policy
 $FipsRegistryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy"
@@ -35,7 +26,4 @@ Write-Host "Running OOBEDeploy tasks..." -ForegroundColor Cyan
 Start-OOBEDeploy
 Write-Log "Start-OOBEDeploy executed"
 
-# Reboot
-Write-Host "OOBE Finalization complete. Restarting..." -ForegroundColor Cyan
-Write-Log "Rebooting device"
-Restart-Computer -Force
+
