d61ff8d233c67ee6a2c3f025abf0cd6173488f4a
dlynch34 <dlynch34@gmail.com>
2025-04-09 14:09:07 -0400

    Update setupcomplete.cmd

--- OSD/setupcomplete.cmd
+++ OSD/setupcomplete.cmd
@@ -1,6 +1,7 @@
 @echo off
-REM === Allow PowerShell scripts to run ===
-powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force"
+REM === Create OSD folder and download script ===
+mkdir %SystemDrive%\OSD
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/dlynch34/test/main/OSD/OOBEFinalize.ps1' -OutFile '%SystemDrive%\OSD\OOBEFinalize.ps1'"
 
-REM === Run OOBEFinalization script from GitHub ===
-powershell.exe -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -NoProfile -Command \"iwr -useb https://raw.githubusercontent.com/dlynch34/test/main/OSD/OOBEFinalize.ps1 | iex\"' -WindowStyle Hidden"
+REM === Run the script with Bypass (ignore execution policy) ===
+powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%SystemDrive%\OSD\OOBEFinalize.ps1"
