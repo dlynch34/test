4773a13d0df8cccb905c8940588554898edf6371
dlynch34 <dlynch34@gmail.com>
2025-06-03 15:16:42 -0400

    Update setupcomplete.cmd

--- OSD/setupcomplete.cmd
+++ OSD/setupcomplete.cmd
@@ -1,7 +1,2 @@
 @echo off
-REM === Create OSD folder and download script ===
-mkdir %SystemDrive%\OSD
-powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/dlynch34/test/main/OSD/OOBEFinalize.ps1' -OutFile '%SystemDrive%\OSD\OOBEFinalize.ps1'"
-
-REM === Run the script with Bypass (ignore execution policy) ===
 powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File "%SystemDrive%\OSD\OOBEFinalize.ps1"
